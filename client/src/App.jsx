4550c8676ee82ee5718236da7332ebb028372d40
Luuka Lindgren <luslin@utu.fi>
2023-05-19 21:40:02 +0300

    Added Todo list

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,18 @@
-import { useState } from 'react'
 import Sidenav from './components/Sidenav'
 import Main from './components/Main'
 import Work from './components/Work'
 import Projects from './components/Projects'
 import Contact from './components/Contact'
+
+// TODO:
+// - Add self portrait and some animated circle around it
+// - Add section for skills, education, and other stuff about me
+// - Change domain to luuka.software (take it from kisakalenteri project)
+// - Make repetetive Tailwind classes into components
+// - Make contact form work, maybe change it to email link
+// - Add dark mode
+// - Publish page on Azure or GitHub pages
+
 
 function App() {
 
