6e904facdbc5a835bb0a2ff1cf11212feceac7c7
Luuka Lindgren <luslin@utu.fi>
2023-05-19 12:54:03 +0300

    Adjusted icon size

--- client/src/components/Sidenav.jsx
+++ client/src/components/Sidenav.jsx
@@ -68,31 +68,31 @@ const Sidenav = () => {
             href="#main"
             className="rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-300"
           >
-            <AiOutlineHome size={40} />
+            <AiOutlineHome size={20} />
           </a>
           <a
             href="#work"
             className="rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-300"
           >
-            <GrProjects size={40} />
+            <GrProjects size={20} />
           </a>
           <a
             href="#projects"
             className="rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-300"
           >
-            <AiOutlineProject size={40} />
+            <AiOutlineProject size={20} />
           </a>
           <a
             href="#resume"
             className="rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-300"
           >
-            <BsPerson size={40} />
+            <BsPerson size={20} />
           </a>
           <a
             href="#contact"
             className="rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-300"
           >
-            <AiOutlineMail size={40} />
+            <AiOutlineMail size={20} />
           </a>
         </div>
       </div>
