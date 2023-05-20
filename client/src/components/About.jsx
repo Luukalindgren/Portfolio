e6e013b6926c30f25cefe6e7f7c7f82352c811d7
Luuka Lindgren <luslin@utu.fi>
2023-05-20 12:31:17 +0300

    Context to About Me

--- client/src/components/About.jsx
+++ client/src/components/About.jsx
@@ -3,15 +3,48 @@ import IMG_0549_lowres from "../assets/IMG_0549_lowres.jpg";
 
 const About = () => {
   return (
-    <div id="about" className="max-w-[1040px] m-auto md:pl-20 p-4 py-16 bg-stone-100">
+    <div
+      id="about"
+      className="max-w-[1040px] m-auto md:pl-20 p-4 py-16 bg-stone-100"
+    >
       <h1 className="py-4 text-4xl font-bold text-center text-[#4B1300]">
         About Me
       </h1>
-      <img
-        className="w-48 h-48 m-auto rounded-full"
-        src={IMG_0549_lowres}
-        alt="Profile Picture"
-      />
+      <div className="flex flex-block ">
+        <img
+          className="w-48 h-48 m-auto rounded-full shadow-lg"
+          src={IMG_0549_lowres}
+          alt="Profile Picture"
+        />
+        <div className="grid flex-auto gap-4 px-8 m-auto sm:grid-cols-3">
+          <div>
+            <ol className="about-list">
+              <h2 className="text-lg font-semibold text-[#4B1300]">
+                Personality
+              </h2>
+              <li>Outgoing</li>
+              <li>Positive</li>
+              <li>Always learning</li>
+            </ol>
+          </div>
+          <div>
+            <ol className="about-list">
+              <h2 className="text-lg font-semibold text-[#4B1300]">Skills</h2>
+              <li>Programming 6/5</li>
+              <li>Design 4/5</li>
+              <li>People skills 0/5</li>
+            </ol>
+          </div>
+          <div>
+            <ol className="about-list">
+              <h2 className="text-lg font-semibold text-[#4B1300]">Hobbies</h2>
+              <li>Gym</li>
+              <li>Reading</li>
+              <li>Disc Golf</li>
+            </ol>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -2,6 +2,13 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    listStyleType: {
+      none: "none",
+      disc: "disc",
+      decimal: "decimal",
+      square: "square",
+      roman: "upper-roman",
+    },
     extend: {},
   },
   plugins: [],
