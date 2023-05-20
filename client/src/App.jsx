1a3bcb704306b00f582f7c8898489874bbcbb46b
Luuka Lindgren <luslin@utu.fi>
2023-05-20 11:41:01 +0300

    Very basic and plain footer

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,6 +4,7 @@ import Work from './components/Work'
 import Projects from './components/Projects'
 import Contact from './components/Contact'
 import About from './components/About'
+import Footer from './components/Footer'
 
 // TODO:
 // - Add self portrait and some animated circle around it
@@ -27,6 +28,7 @@ function App() {
       <Work/>
       <Projects/>
       <Contact />
+      <Footer/>
     </div>
   )
 }
---
+++ client/src/components/Footer.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div
+      id="footer"
+      className="w-full p-4 py-16 m-auto md:pl-20 bg-stone-200"
+    >
+      <p className="text-center text-stone-500">© 2023 Luuka Lindgren</p>
+    </div>
+  );
+};
+
+export default Footer;
