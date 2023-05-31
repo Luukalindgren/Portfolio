33a8c4da5dc88ef2efa569b65990bb1e9faf9944
Luuka Lindgren <luslin@utu.fi>
2023-05-31 12:26:59 +0300

    Update TODO

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,10 @@
-import Sidenav from './components/Sidenav'
-import Main from './components/Main'
-import Work from './components/Work'
-import Projects from './components/Projects'
-import Contact from './components/Contact'
-import About from './components/About'
-import Footer from './components/Footer'
+import Sidenav from "./components/Sidenav";
+import Main from "./components/Main";
+import Work from "./components/Work";
+import Projects from "./components/Projects";
+import Contact from "./components/Contact";
+import About from "./components/About";
+import Footer from "./components/Footer";
 
 // TODO:
 // DONE (without animation) Add self portrait and some animated circle around it
@@ -14,27 +14,26 @@ import Footer from './components/Footer'
 // DONE Make repetetive Tailwind classes into components
 // DONE Publish page on Azure
 // DONE Make pop-up for sending email
-// - Make SEO better, so that it shows up on google search (example: "Luuka Lindgren" search should show this page)
+// - Add prevention for empty fields in contact form (ex. email field gives error when in wrong format, but not when empty)
+// - Make SEO better, so that it shows up on google search (ex. "Luuka Lindgren" search should show this page)
 // - Refine GitHub repo, finish README.md etc.
 // - Make sure that domain works properly, (luuka.software and www.luuka.software)
 // - Add dark mode
 // - Add comments to code
 // - Add some animations or other fancy stuff, but keep it simple
 
-
 function App() {
-
   return (
     <div>
       <Sidenav />
       <Main />
-      <About/>
-      <Work/>
-      <Projects/>
+      <About />
+      <Work />
+      <Projects />
       <Contact />
-      <Footer/>
+      <Footer />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
