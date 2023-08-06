eb951f7c178810154e2902678a2bfcfba8b17c06
luukalindgren <luslin@utu.fi>
2023-08-06 14:18:26 +0300

    Updated projects

--- client/src/components/Projects.jsx
+++ client/src/components/Projects.jsx
@@ -4,6 +4,7 @@ import kisakalenteriappImg from "../assets/kisakalenteri-app-github.png";
 import githubImg from "../assets/github.png";
 import kisakalenteriImg from "../assets/kisakalenteri.png";
 import BlogImg from "../assets/Blogi.png";
+import LitenDevImg from "../assets/LitenDev_homepage.png"
 
 const Projects = () => {
   return (
@@ -14,7 +15,7 @@ const Projects = () => {
           img={kisakalenteriImg}
           title="Competition calendar"
           desc="MERN -stack project"
-          link="https://luukalindgren.github.io/Competition-calendar/"
+          link="https://github.com/Luukalindgren/Competition-calendar"
         />
         <ProjectItem
           img={BlogImg}
@@ -29,11 +30,22 @@ const Projects = () => {
           link="https://github.com/Luukalindgren/kisakalenteri-app"
         />
         <ProjectItem
-          img={githubImg}
-          title="GitHub"
-          desc="All projects"
-          link="https://github.com/Luukalindgren"
+          img={LitenDevImg}
+          title="LitenDev"
+          desc="Webflow project"
+          link="https://liten.dev"
         />
+      </div>
+      <div className="relative flex items-center justify-center pt-10 ">
+        <a
+          href="https://github.com/Luukalindgren"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <p className="p-3 text-lg font-semibold text-white bg-[#4B1300] rounded-lg cursor-pointer px-8 relative ">
+            All projects
+          </p>
+        </a>
       </div>
     </div>
   );
