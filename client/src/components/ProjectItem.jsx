24733168cebe789606279c8e0d89eeeb414105bf
luukalindgren <luslin@utu.fi>
2025-03-12 21:24:09 +0200

    2nd link + styling

--- client/src/components/ProjectItem.jsx
+++ client/src/components/ProjectItem.jsx
@@ -1,10 +1,18 @@
 import React from "react";
 
-const ProjectItem = ({ img, title, desc, link, showOverlay, onClick }) => {
+const ProjectItem = ({
+  img,
+  title,
+  desc,
+  sourceLink,
+  secondaryLink,
+  showOverlay,
+  onClick,
+}) => {
   const overlayStyle = showOverlay
     ? "opacity-100 visible"
     : "opacity-0 invisible";
-  
+
   return (
     <div className="duration-200 shadow-xl shadow-gray-400 rounded-xl group md:hover:scale-105">
       <div className="relative w-full aspect-video">
@@ -15,7 +23,7 @@ const ProjectItem = ({ img, title, desc, link, showOverlay, onClick }) => {
           onClick={() => onClick(title)}
         />
         <div
-          className={`absolute inset-0 flex items-center justify-center text-center transition-opacity duration-200 ${overlayStyle} bg-gradient-to-r from-gray-500/20 to-[#4B1300]/80 rounded-xl md:group-hover:opacity-100 md:group-hover:visible`}
+          className={`absolute inset-0 flex items-center justify-center text-center transition-opacity duration-200 ${overlayStyle} bg-gradient-to-r from-gray-800/30 to-[#4B1300]/80 rounded-xl md:group-hover:opacity-100 md:group-hover:visible`}
           onClick={() => onClick(title)}
         >
           <div>
@@ -23,16 +31,32 @@ const ProjectItem = ({ img, title, desc, link, showOverlay, onClick }) => {
               {title}
             </h3>
             <p className="pt-2 pb-4 text-white">{desc}</p>
-            <a
-              href={link}
-              target="_blank"
-              rel="noopener noreferrer"
-              className="inline-block w-[200px] hover:scale-105 ease-in duration-200"
-            >
-              <p className="p-3 text-lg font-bold text-gray-700 bg-white rounded-lg cursor-pointer">
-                More info
-              </p>
-            </a>
+            <div className="inline-flex gap-3">
+              {sourceLink && (
+                <a
+                  href={sourceLink}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="inline-block duration-200 ease-in hover:scale-105"
+                >
+                  <p className="p-3 text-lg font-bold text-gray-700 bg-white rounded-lg cursor-pointer min-w-[100px]">
+                    Source
+                  </p>
+                </a>
+              )}
+              {secondaryLink && (
+                <a
+                  href={secondaryLink}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="inline-block duration-200 ease-in hover:scale-105"
+                >
+                  <p className="p-3 text-lg font-bold text-gray-700 bg-white rounded-lg cursor-pointer min-w-[100px]">
+                    Website
+                  </p>
+                </a>
+              )}
+            </div>
           </div>
         </div>
       </div>
